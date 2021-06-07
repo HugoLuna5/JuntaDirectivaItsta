1bda59d8ccf2e5d652fd638755dbbac74ef93c8f
Hugo Luna <hugo_1199@hotmail.com>
2021-06-07 02:45:14 -0500

    Added ui login and register and added routes to auth

--- src/database.js
+++ src/database.js
@@ -9,3 +9,21 @@ mongoose.connect('mongodb://localhost/juntapp', {
 .catch(err => console.error(err));
 
 
+/**
+ import mongoose from "mongoose";
+import config from "./config";
+
+(async () => {
+  try {
+    const db = await mongoose.connect(config.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("Mongodb is connected to", db.connection.host);
+  } catch (error) {
+    console.error(error);
+  }
+})();
+ */
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,36 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-
-router.get('/users/signin', (req, res) => {
-
+router.get("/users/signin", (req, res) => {
+  res.render("users/signin");
 });
 
-router.get('/users/signup', (req, res) => {
-
+router.get("/users/signup", (req, res) => {
+  res.render("users/signup");
 });
 
+router.post("/users/signin", (req, res) => {
+  const { name, email, password, confirm_password } = req.body;
+
+  const errors = [];
+
+  if (password != confirm_password) {
+    errors.push({ text: "Las contraseñas no coinciden" });
+  }
+
+  if (password.length < 4) {
+    errors.push({ text: "Las contraseñas deben tener al menos 4 caracteres." });
+  }
+
+  if (errors.length > 0) {
+    res.render("users/signup", {
+      errors,
+      name,
+      email,
+      password,
+      confirm_password,
+    });
+  } else {
+  }
+});
 
 module.exports = router;
